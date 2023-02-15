0eaa03c56784b957d51d5964a4f597159e2fe6de
abhradipg <abhradip.ghosh9@gmail.com>
2023-02-15 22:47:23 +0530

    final??

--- proj1/MaxPool.py
+++ proj1/MaxPool.py
@@ -24,7 +24,7 @@ def main(args):
     command = "nvidia-smi dmon -o T -c 1| grep 0"
     print_gpu_stat(command)
     time.sleep(2);
-    model = nn.MaxPool2d(kernel_size=3, padding=1).to(device)
+    model = nn.MaxPool2d(kernel_size=2, stride=2).to(device)
     x = torch.randn(inpSize).to(device)
     y = model(x)
     torch.cuda.synchronize()
